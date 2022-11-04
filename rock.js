90cd0170fd52b705c6438a13745679dcb2efc2f8
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-11-04 11:52:33 -0500

    creates single round

--- rock.js
+++ rock.js
@@ -8,5 +8,23 @@ function getComputerChoice() {
         return 'scissors'
     }
 }
-let computerSelection = getComputerChoice();
-console.log(computerSelection)
+function singleRound (computerSelection, playerSelection) {
+    if (computerSelection === playerSelection.toLowerCase()) {
+        return 'It\'s a tie!'
+    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') {
+        return 'You lose! Rock beats scissors.'
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') {
+        return 'You lose! Paper beats scissors.'
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
+        return 'You lose! Scissors beat paper.'
+    }else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
+        return 'You win! Paper beats rock.'
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
+        return 'You win! scissors beat paper.'
+    } else {
+        return 'You win! Rock beats scissors'
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(singleRound(playerSelection, computerSelection));
