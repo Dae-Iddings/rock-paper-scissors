b2a2b0427065046168a9760cbad4b6c63d90f6e3
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-11-04 16:50:32 -0500

    allows for random answer each round

--- rock.js
+++ rock.js
@@ -1,30 +1,36 @@
-let randomNumber = Math.floor(Math.random() * 100);
-function getComputerChoice() {
-    if (randomNumber <= 33) {
-        return 'rock';
-    } else if (randomNumber >= 66) {
-        return 'paper';
-    } else {
-        return 'scissors';
+
+for (let i = 0; i < 5; i++) {
+    let randomNumber = Math.floor(Math.random() * 100);
+    function getComputerChoice() {
+        if (randomNumber <= 33) {
+            return 'rock';
+        } else if (randomNumber >= 66) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
     }
-}
-function singleRound (computerSelection, playerSelection) {
-    if (computerSelection === playerSelection.toLowerCase()) {
-        return 'It\'s a tie!';
-    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') {
-        return 'You lose! Rock beats scissors.';
-    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
-        return 'You lose! Paper beats scissors.';
-    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
-        return 'You lose! Scissors beat paper.';
-    }else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
-        return 'You win! Paper beats rock.';
-    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
-        return 'You win! scissors beat paper.';
-    } else {
-        return 'You win! Rock beats scissors';
+
+    function singleRound(computerSelection2, playerSelection2) {
+        if (computerSelection2 === playerSelection2.toLowerCase()) {
+            return 'It\'s a tie!';
+        } else if (computerSelection2 === 'rock' && playerSelection2.toLowerCase() === 'scissors') {
+            return 'You lose! Rock beats scissors.';
+        } else if (computerSelection2 === 'paper' && playerSelection2.toLowerCase() === 'rock') {
+            return 'You lose! Paper beats rock.';
+        } else if (computerSelection2 === 'scissors' && playerSelection2.toLowerCase() === 'paper') {
+            return 'You lose! Scissors beat paper.';
+        } else if (computerSelection2 === 'rock' && playerSelection2.toLowerCase() === 'paper') {
+            return 'You win! Paper beats rock.';
+        } else if (computerSelection2 === 'paper' && playerSelection2.toLowerCase() === 'scissors') {
+            return 'You win! scissors beat paper.';
+        } else if (computerSelection2 === 'scissors' && playerSelection2.toLowerCase() === 'rock') {
+            return 'You win! Rock beats scissors';
+        } else {
+            return 'enter a valid answer'
+        }
     }
+    let computerSelection = getComputerChoice();
+    let playerSelection = window.prompt("Enter rock, paper, or scissors.")
+    console.log(singleRound(computerSelection, playerSelection))
 }
-let playerSelection = prompt('Rock? Paper? Scissors?');
-const computerSelection = getComputerChoice();
-console.log(singleRound(computerSelection, playerSelection.toLocaleLowerCase(),));
