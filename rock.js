7be4d3c75b4ce13538d9c886cb88891033e9f78e
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-11-04 12:17:52 -0500

    add player selection prompt

--- rock.js
+++ rock.js
@@ -3,28 +3,28 @@ function getComputerChoice() {
     if (randomNumber <= 33) {
         return 'rock';
     } else if (randomNumber >= 66) {
-        return 'paper'
+        return 'paper';
     } else {
-        return 'scissors'
+        return 'scissors';
     }
 }
 function singleRound (computerSelection, playerSelection) {
     if (computerSelection === playerSelection.toLowerCase()) {
-        return 'It\'s a tie!'
+        return 'It\'s a tie!';
     } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') {
-        return 'You lose! Rock beats scissors.'
-    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') {
-        return 'You lose! Paper beats scissors.'
-    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
-        return 'You lose! Scissors beat paper.'
-    }else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
-        return 'You win! Paper beats rock.'
+        return 'You lose! Rock beats scissors.';
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
-        return 'You win! scissors beat paper.'
+        return 'You lose! Paper beats scissors.';
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
+        return 'You lose! Scissors beat paper.';
+    }else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
+        return 'You win! Paper beats rock.';
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
+        return 'You win! scissors beat paper.';
     } else {
-        return 'You win! Rock beats scissors'
+        return 'You win! Rock beats scissors';
     }
 }
-const playerSelection = "rock";
+let playerSelection = prompt('Rock? Paper? Scissors?');
 const computerSelection = getComputerChoice();
-console.log(singleRound(playerSelection, computerSelection));
+console.log(singleRound(computerSelection, playerSelection.toLocaleLowerCase(),));
